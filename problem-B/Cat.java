7210673822f974ab175f7c222bc5fb0cba5ea705
meirzhan <124430073+meirzhan-anarbekuly@users.noreply.github.com>
2023-02-25 23:49:51 +0600

    Update Cat.java

--- problem-B/Cat.java
+++ problem-B/Cat.java
@@ -1,1 +1,32 @@
+public class Cat extends Animal implements Pet {
+    private String name;
 
+    public Cat(String name) {
+        super(4);
+        this.name = name;
+    }
+
+    public Cat() {
+        this("Kitty");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("A cat named " + this.name + " is eating kitekat!");
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("A cat named " + this.name + " is playing!");
+    }
+}
