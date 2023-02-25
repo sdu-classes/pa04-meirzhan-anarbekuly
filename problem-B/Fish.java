5296f8162c7aff1348bbdc55f3f2cf78e011ad39
meirzhan <124430073+meirzhan-anarbekuly@users.noreply.github.com>
2023-02-25 23:50:15 +0600

    Update Fish.java

---
+++ problem-B/Fish.java
@@ -0,0 +1,37 @@
+public class Fish extends Animal implements Pet {
+    private String name;
+
+    public Fish(String name) {
+        super(0);
+        this.name = name;
+    }
+
+    public Fish() {
+        this("Nemo");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Fish named " + this.name + " is eating!");
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Fish named " + this.name + " is playing!");
+    }
+
+    @Override
+    public void walk() {
+        throw new UnsupportedOperationException("The fish can't walk!");
+    }
+}
